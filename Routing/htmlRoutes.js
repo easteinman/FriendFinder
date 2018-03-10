882af83a5661824f45f139eb05a65c51b80b6c33
Eric <ericsteinman@me.com>
2018-03-09 22:43:23 -0600

    Final commit

--- Routing/htmlRoutes.js
+++ Routing/htmlRoutes.js
@@ -7,7 +7,7 @@ var path = require('path');
 module.exports = function(app) {
 
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname + '/../docs', "home.html"));
+        res.sendFile(path.join(__dirname + '/../docs', "index.html"));
     });
 
     app.get("/survey", function(req, res) {
